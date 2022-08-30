58e408830ab05232612fd92b723984f58ed1a25b
Oussama Jabrane <oussamajabrane@MacBook-Pro-de-Oussama.local>
2022-08-30 23:43:15 +0100

    Loading functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Offline from "./Offline.jsx"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Offline from "./Offline.jsx";
 
-
-function getRandom (max, min) {
+function getRandom(max, min) {
   const number = Math.floor(Math.random() * (max - min));
-  return number
+  return number;
 }
-
 
 function App() {
   const range = getRandom(711, 1);
   const [facts, setFacts] = useState();
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': '466e204c27msh426d08ada9700f7p1c83b6jsn5df82df1c172',
-      'X-RapidAPI-Host': 'fun-facts-generator.p.rapidapi.com'
-    }
+      "X-RapidAPI-Key": "466e204c27msh426d08ada9700f7p1c83b6jsn5df82df1c172",
+      "X-RapidAPI-Host": "fun-facts-generator.p.rapidapi.com",
+    },
   };
   const getApiData = async () => {
     const response = await fetch(
-      `https://fun-facts-generator.p.rapidapi.com/${range}`, options
-    ).then((response) => response.json());
+      `https://fun-facts-generator.p.rapidapi.com/${range}`,
+      options
+    ).then(response => response.json());
     setFacts(response.fact);
   };
   useEffect(() => {
     getApiData();
-  }, [])
+  }, []);
   return (
     <>
       <div className="card">
         <div className="inner-card">
-          <h1>{facts ? <>{facts}</> : <Offline />}</h1>
+          <h1>{facts ? <>{facts}</> : "Loading..."}</h1>
         </div>
       </div>
     </>
   );
-};
+}
 
 export default App;
